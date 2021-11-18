64936253952ce780f88405bf9e2c1c75e6f9fef7
JeevanJoshi01 <94644653+JeevanJoshi01@users.noreply.github.com>
2021-11-19 02:29:30 +0530

    last patch update

--- script/button-work.js
+++ script/button-work.js
@@ -1,7 +1,22 @@
-const infoIcon = document.getElementById("icon-info");
+let infoIcon = document.getElementById("icon-info");
 const infoBtn = document.getElementById("btn-info");
-const fileIcon = document.getElementById("icon-camera");
+let fileIcon = document.getElementById("icon-camera");
 const fileBtn = document.getElementById("btn-camera");
+let firstNum = document.getElementById("first-tel");
+let SecondNum = document.getElementById("second-tel");
+let insta = document.getElementById("Insta");
+
+insta.addEventListener('click', ()=>{
+    window.open('https://instagram.com/iam_jeevan_joshi', '_blank');
+})
+
+firstNum.addEventListener("click", ()=>{
+    window.open('tel:+917037675604', '_self');
+})
+
+SecondNum.addEventListener("click", ()=>{
+    window.open('tel:+918477848314', '_self');
+})
 
 infoIcon.addEventListener("click", ()=>{
     if(infoBtn.style.visibility == "hidden"){
